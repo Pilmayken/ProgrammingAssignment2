fb0226e5e3022052e66b014963327887ec0df4b3
Carolina <carolina.avilesf@gmail.com>
2017-12-02 11:15:11 -0400

    Assignment: Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The first function, makeVector creates a special "vector", which is really a list containing a function to
+#set the value of the vector
+#set the value of the vector
+#set the value of the mean
+#get the value of the mean
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL         # valor de la matriz inversa
+  
+  set <- function(y) {    # establecer valor de la matriz
+    x <<- y
+    inverse <<- NULL
+  }                        
+  
+  get <- function() x       # obtener valor de la matriz
+  
+  setInverse <- function(inverseInput) inverse <<- inverseInput   # establecer el valor de la inversa
+
+  getInverse <- function() inverse    # obtener el valor de la inversa
+
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)    # devolver la lista de funciones
+
+
 }
 
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getInverse()     # revisar si ya entrega la inversa la funcion anterior
+  
+  if(!is.null(inverse)){        # obtener la inversa
+
+    return(inverse)
+  }
+  
+  data <- x$get()          #obtener la matriz
+  
+  inverse <- solve(data)   #calcular la inversa con la funcion "solve()"
+  
+  x$setInverse(inverse)    # obtener la inversa
+  
+  inverse                 # entregar el resultado
+  
 }
